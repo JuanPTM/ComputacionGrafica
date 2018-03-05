e7846df8ec4d3c75e46b6ecf983cda6c4ecba571
ibarbech <ibarbech@alumnos.unex.es>
2018-03-05 20:57:57 +0100

    update methods

--- main.py
+++ main.py
@@ -60,21 +60,30 @@ def GenerateCodebook(descriptors):
     return codebook, dev
 
 def GenerateAllHistograms():
+    allHistograms =[]
     orb = cv2.ORB_create()
+    allDescriptors = None
     for path in nameVideos:
         histograms = None
-        print "inicio el video", pathVideos+path+extension
+        print pathVideos+path+extension
         capture = cv2.VideoCapture(pathVideos+path+extension)
         while True:
             # Capturamos
             ret, frame = capture.read()
             if ret == False:
                 break
-            # Escalamos
+
             # Convertimos a gris
-            gray = cv2.cvtColor(resized, cv2.COLOR_BGR2GRAY)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+            # Recortamos
+            graycrop = cropToRequirements(gray)
+
+            # Escalamos
+            grayScale = scale(graycrop)
+
             # Sacamos keypoints y descriptores
-            keypoints, descriptors = orb.detectAndCompute(gray, None)
+            keypoints, descriptors = orb.detectAndCompute(grayScale, None)
 
             if descriptors is not None:
                 histogram, limites = np.histogram(vq(descriptors/dev, codebook)[:][1],bins=range(21))
@@ -82,7 +91,8 @@ def GenerateAllHistograms():
                     histograms = histogram
                 else:
                     histograms = np.vstack((histograms, histogram))
-
+        allHistograms.append(histograms)
+    return allHistograms
 
 def SegmentFrame(frameGray, prevframeGray):
     m = np.subtract(prevframeGray.astype(np.int16),frameGray.astype(np.int16))
@@ -106,15 +116,37 @@ def morphFilters(binFrame):
     return dilateImage
 
 def extractIntReg(binFrame, padding=10, minCont=50):
-    aux = np.copy(binFrame)
     regions = []
-    image,contours,hierarchy = cv2.findContours(binFrame,cv2.RETR_LIST,cv2.CHAIN_APROX_SIMPLE)
-
+    image,contours,hierarchy = cv2.findContours(binFrame,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
     for cont in contours:
-        if len(cont) > minCont:
+        if len(cont) > minCont and cv2.contourArea(cont)>2500:
             x,y,w,h = cv2.boundingRect(cont)
-            regions.append((x-padding,y-padding,w+2*padding,h+2*padding))
-    return regions
+            paddingx = paddingy = paddingw = paddingh = 10
+            if x < 10:
+                paddingx = x
+            if y < 10:
+                paddingy = y
+            if x+w+padding > image.shape[1]:
+                paddingw = image.shape[1]-x-w
+            if y+h+padding > image.shape[0]:
+                paddingh = image.shape[0]-y-h
+            regions.append((x-paddingx,y-paddingy, w+paddingx+paddingw, h+paddingy+paddingh))
+    return regions,contours
+
+def union(a,b):
+  x = min(a[0], b[0])
+  y = min(a[1], b[1])
+  w = max(a[0]+a[2], b[0]+b[2]) - x
+  h = max(a[1]+a[3], b[1]+b[3]) - y
+  return (x, y, w, h)
+
+def intersection(a,b):
+  x = max(a[0], b[0])
+  y = max(a[1], b[1])
+  w = min(a[0]+a[2], b[0]+b[2]) - x
+  h = min(a[1]+a[3], b[1]+b[3]) - y
+  if w<0 or h<0: return None # or (0,0,0,0) ?
+  return (x, y, w, h)
 
 if __name__ == "__main__":
     try:
@@ -127,13 +159,11 @@ if __name__ == "__main__":
         pickle.dump(dev, open('dev.pickle', 'w'))
         pickle.dump(codebook, open('codebook.pickle', 'w'))
         print "He terminado"
-    # try:
-    #
-    # except Exception as e:
-    #
-    #     histograms = GenerateAllHistograms()
-    #     pickle.dump(histograms, open(path[:-5]+'histograms.pickle', 'w'))
-    #     raise
+    try:
+        histograms = pickle.load(open('allHistograms.pickle', 'r'))
+    except:
+        allHistograms = GenerateAllHistograms()
+        pickle.dump(allHistograms, 'allHistograms.pickle', 'w'))
     capture = cv2.VideoCapture("movies/test.webm")
     while True:
         # Capturamos
@@ -154,7 +184,15 @@ if __name__ == "__main__":
 
         procesedImage = morphFilters(binImg)
 
-        cv2.imshow("d", procesedImage)
+        regions, contornos = extractIntReg(procesedImage)
+        cv2.drawContours(grayScale, contornos, -1, (0,255,0), 3)
+
+        show_image = np.copy(procesedImage)
+
+        for r in regions:
+            cv2.rectangle(grayScale,(r[0],r[1]),(r[0]+r[2],r[1]+r[3]),(255,0,0))
+
+        cv2.imshow("d", grayScale)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
